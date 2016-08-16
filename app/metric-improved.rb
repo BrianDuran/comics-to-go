f2b6916139339081071c0ff009cbbe1129eb321f
BrianDuran <bduran@growthaccelerationpartners.com>
2016-08-15 18:56:26 -0600

    add class with corrections

---
+++ app/metric-improved.rb
@@ -0,0 +1,44 @@
+class Metric
+
+  def initialize values
+     @values = values
+  end
+
+  def get_values
+    "Values: #{ @values.join("-")}"
+  end
+
+#Calculates the average value
+  def average
+    total = @values.reduce(:+)
+    return "Average #{total/@values.size}"
+  end
+
+
+  def odds
+    odds = @values.select{|num| num.odd?}
+    "Odds: #{odds.join('-')}"
+  end
+
+  def addvalue value
+    if (0..100).include? value
+      @values << value
+    end
+  end
+
+
+  def hash
+    {
+      first: @values.first,
+      last: @values.last
+    }
+  end
+end
+
+m = Metric.new([10, 3, 5, 6, 8, 8])
+puts m.get_values
+m.addvalue 15
+puts m.get_values
+puts m.average
+puts m.odds
+puts m.hash
---
+++ app/metric.rb
@@ -0,0 +1,63 @@
+class Metric
+
+  def initialize values
+     @values = values
+  end
+
+  def get_values
+    "Values: " + @values.join("-"); # "Values: #{ @values.join("-")}"
+  end
+
+#Calculates the average value
+  def average
+    total = 0
+    metricsValues = @values
+    metricsValues.each { |metric|
+      total = total+metric
+    }
+    average = total / metricsValues.size
+    return "Average: "+average.to_s
+=begin
+  total = @values.reduce(:+)
+  return "Average #{total/@values.size}"
+=end
+
+  end
+
+
+  def odds
+    odds = @values.select do |value|
+      if value % 2 == 0
+        false
+      else
+        true
+       end
+    end
+    "Odds: " + odds.join('-')
+  end
+
+  def addvalue (value) #def addvalue value ?
+    if value > 0 and value < 100
+      @values << value
+    end
+  end
+
+
+  def hash
+    { "first" => @values[0],
+      "last" => @values[@values.size - 1]
+    }
+=begin
+  { first: @values[0],
+    last: @values[@values.size - 1] }
+=end
+  end
+end
+
+m = Metric.new([10, 3, 5, 6, 8, 8])
+puts m.get_values
+m.addvalue 15
+puts m.get_values
+puts m.average
+puts m.odds
+puts m.hash
